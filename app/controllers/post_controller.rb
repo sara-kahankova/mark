4eb6bf01a87bfa388ca3efb87e8115ac5e40726b
Sara Kahankova <75204851+kahinek1999@users.noreply.github.com>
2022-01-15 09:46:34 -0500

    posts controller actions done

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,44 @@
 class PostController < ApplicationController
+  before_action :find_post, only: %i(edit show update destroy)
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @post.update(post_params)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def show; end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
