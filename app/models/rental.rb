28c9e7c00f409e0990d6eb43cc92af1f86840d1d
Sara Kahankova <75204851+kahinek1999@users.noreply.github.com>
2022-01-16 09:44:35 -0500

    data validations done

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,8 @@
 class Rental < ApplicationRecord
+  validates :title, presence :true
+  validates :address, presence :true
+  validates :description, presence :true
+  validates :price, presence :true
+  validates :size, presence :true
+  validates :city, presence :true
 end
