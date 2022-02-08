b8437f23b2b9da514b47537ad332aa0254ac9bd5
Maryan Berhe <maryanberhe@gmail.com>
2022-02-07 19:40:28 -0600

    I've added a few fields to the rental model as well as updating the seeds and cards scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_165131) do
+ActiveRecord::Schema.define(version: 2022_02_08_005306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2022_01_29_165131) do
     t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "bed"
+    t.integer "bath"
   end
 
   create_table "reviews", force: :cascade do |t|
